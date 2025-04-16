97ef0d12f2798510ae39827206da02eb659f4511
Whee30 <forrest.cook@gmail.com>
2025-04-15 21:35:54 -0700

    modified:   Potato/iterate_blobs.py

--- Potato/iterate_blobs.py
+++ Potato/iterate_blobs.py
@@ -1,7 +1,27 @@
 import sqlite3
 from datetime import datetime
+import argparse
+import sys
 
-connection = sqlite3.connect('tgdata.db', uri=True)
+user_input = argparse.ArgumentParser(description="Call the script, target your tgdata.db database and specify an output directory. Optionally provide a user ID OR a group ID to target one of those.")
+
+# Define the expected flag and argument
+user_input.add_argument("-d", "--database", required=True, help="Path to the input file")
+user_input.add_argument("-o", "--output", required=True, help="Path to the output directory")
+
+parse_method = user_input.add_mutually_exclusive_group(required=False)
+parse_method.add_argument("-u", "--user", required=False, help="Optional user ID")
+parse_method.add_argument("-c", "--chat", required=False, help="Optional chat ID")
+
+# Parse the command-line arguments
+args = user_input.parse_args()
+
+if not args.file:
+    print("Error: a database must be specified. A specific user OR a specific chat may also be specified by using -u OR -c, respectively.")
+    print("Usage: python script.py -d <database file> -o <output directory>")
+    sys.exit(1)
+
+connection = sqlite3.connect(user_input.file, uri=True)
 connection.row_factory = sqlite3.Row
 cur = connection.cursor()
 # Choose the table and column here
@@ -28,10 +48,31 @@ for each_row in rows:
     extracted_int = int.from_bytes(extracted_bytes, byteorder='little')
     user_ids.append(extracted_int)
 
-for index, each_id in enumerate(user_ids):
-    if each_id == 94276828: # Change this value to your target User ID
-        entry = index + 1
-        filename = str(entry) + '_' + str(group_ids[index]) # filenames will be [database entry #]_[group ID]
-        print(f"Match found in row: {index + 1}")
-        with open(filename, 'wb') as file: # extract the BLOBs for further examination 
-            file.write(rows[index][0])
+
+
+if user_input.user: # Iterate all of the BLOBs from a specific user
+    for index, each_id in enumerate(user_ids):
+        if each_id == user_input.user: # Optional user ID supplied by -u/--user flag
+            entry = index + 1
+            filename = str(entry) + '_' + str(group_ids[index]) # filenames will be [database entry #]_[group ID]
+            print(f"Match found in row: {index + 1}")
+            with open(filename, 'wb') as file: # extract the BLOBs for further examination 
+                file.write(rows[index][0])
+                
+elif user_input.chat: # iterate all of the BLOBs from a specific chat
+    for index, each_id in enumerate(user_ids):
+        if each_id == user_input.user: # Optional user ID supplied by -u/--user flag
+            entry = index + 1
+            filename = str(entry) + '_' + str(group_ids[index]) # filenames will be [database entry #]_[group ID]
+            print(f"Match found in row: {index + 1}")
+            with open(filename, 'wb') as file: # extract the BLOBs for further examination 
+                file.write(rows[index][0])
+                
+else: # iterate all of the blobs from the database
+    for index, each_id in enumerate(user_ids):
+        if each_id == user_input.user: # Optional user ID supplied by -u/--user flag
+            entry = index + 1
+            filename = str(entry) + '_' + str(group_ids[index]) # filenames will be [database entry #]_[group ID]
+            print(f"Match found in row: {index + 1}")
+            with open(filename, 'wb') as file: # extract the BLOBs for further examination 
+                file.write(rows[index][0])
